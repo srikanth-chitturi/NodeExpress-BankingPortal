826eab6b7d89f95d25619f568f5a44ebfe7ab630
srikanth-chitturi <ysrikanth2011@gmail.com>
2020-05-23 12:32:04 +0530

    Completed module 1

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+console.log(`Setting views Directory :  ${path.join(__dirname + '/views')} `);
+app.set('views',path.join(__dirname + '/views'));
+app.set('view engine','ejs');
+
+app.set(express.static('public'));
+
+
+app.get('/',function(req,res){
+    res.render('index',{title: 'Index'});
+});
+
+app.listen(3000,()=>{
+    console.log('PS Project Running on port 3000!');    
+});
